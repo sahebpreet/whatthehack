8bcbac5dd641f6b3d92607363511f00b033889c9
Sudiksha <sudiksha@L-9PRQ542.jasperindia.local>
2015-09-04 21:05:16 +0530

    adding listdaoimpl

--- src/main/java/com/sd/dao/listItemsDao.java
+++ src/main/java/com/sd/dao/listItemsDao.java
@@ -1,9 +1,15 @@
 package com.sd.dao;
 
+import java.util.List;
+
 import com.sd.pojo.Listitems;
+import com.sd.pojo.Product;
+import com.sd.pojo.Shoppinglist;
 
 public interface listItemsDao {
 
-	public Listitems getList(int id);
-	public boolean insertList(Listitems list);
+	public List<Product> getListItems(Shoppinglist list);
+	public void addItem(Shoppinglist list, Product product);
+	public void deleteItem(Shoppinglist list, Product product);
+
 }
--- src/main/java/com/sd/daoimpl/listItemsDaoImpl.java
+++ src/main/java/com/sd/daoimpl/listItemsDaoImpl.java
@@ -1,5 +1,39 @@
 package com.sd.daoimpl;
 
-public class listItemsDaoImpl {
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.sd.dao.listItemsDao;
+import com.sd.pojo.Product;
+import com.sd.pojo.Shoppinglist;
+
+public class listItemsDaoImpl  implements listItemsDao {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public List<Product> getListItems(Shoppinglist list) {
+		String query = "select * from product join listitems on product.id=listitems.pid where list_id=?";
+		List<Product> products=jdbcTemplate.query(query, new Object[] {list.getId()}, new BeanPropertyRowMapper<Product>(Product.class));
+		return products;
+	}
+
+	public void addItem(Shoppinglist list, Product product) {
+		String query="insert into listitems(list_id,pid) values(?,?)";
+		jdbcTemplate.update(query, list.getId(), product.getId());
+	
+	}
+
+	public void deleteItem(Shoppinglist list, Product product) {
+		String query="delete from listitems where list_id=? and pid=?";
+		jdbcTemplate.update(query, list.getId(), product.getId());
+	
+	}
+
+	
 }
+	
